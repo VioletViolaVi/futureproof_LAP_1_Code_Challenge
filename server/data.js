a9133a2c6bddb5dd555d26e9beeb30bf50607ec8
vivian <vivian.v18723@gmail.com>
2022-07-20 16:31:59 +0100

    add cat data obj

---
+++ server/data.js
@@ -0,0 +1,74 @@
+const catData = [
+  {
+    id: 1,
+    breed: "persian",
+    persian:
+      "https://thumbs.dreamstime.com/b/persian-cat-3-years-old-18444682.jpg",
+    name: "Ginger",
+  },
+  {
+    id: 2,
+    breed: "persian",
+    persian:
+      "https://thumbs.dreamstime.com/b/front-view-grumpy-persian-cat-sitting-37852767.jpg",
+    name: "Owlee",
+  },
+  {
+    id: 3,
+    breed: "persian",
+    persian:
+      "https://thumbs.dreamstime.com/b/persian-cat-9-months-old-13667565.jpg",
+    name: "Mr Miyagi",
+  },
+  {
+    id: 4,
+    breed: "persian",
+    persian:
+      " https://thumbs.dreamstime.com/b/crossbreed-siberian-cat-persian-cat-6610485.jpg",
+    name: "Bubbles",
+  },
+  {
+    id: 5,
+    breed: "persian",
+    persian:
+      "https://thumbs.dreamstime.com/b/persian-cat-8-months-old-sitting-18989882.jpg",
+    name: "Cheddar",
+  },
+  {
+    id: 6,
+    breed: "persian",
+    persian:
+      "https://thumbs.dreamstime.com/b/persian-cat-6-months-old-standing-16822105.jpg",
+    name: "Fishbait",
+  },
+  {
+    id: 7,
+    breed: "persian",
+    persian:
+      "https://thumbs.dreamstime.com/b/adorable-persian-cat-27596772.jpg",
+    name: "Jiggles",
+  },
+  {
+    id: 8,
+    breed: "persian",
+    persian:
+      "https://thumbs.dreamstime.com/b/persian-cat-8-months-old-sitting-18989875.jpg",
+    name: "Katy Purry",
+  },
+  {
+    id: 9,
+    breed: "persian",
+    persian:
+      "https://thumbs.dreamstime.com/b/persian-cat-3-months-old-sitting-16409114.jpg",
+    name: "Kit-Kat",
+  },
+  {
+    id: 10,
+    breed: "persian",
+    persian:
+      "https://cattime.com/assets/uploads/gallery/peterbald-mixed-cat-breed-pictures/peterbald-mixed-cat-breed-pictures-4.jpg",
+    name: "Meowise",
+  },
+];
+
+module.exports = catData;
