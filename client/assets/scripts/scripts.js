b611082280079e1af0f6559712f28f3b9ee371df
vivian <vivian.v18723@gmail.com>
2022-07-20 18:00:41 +0100

    update script file

--- client/assets/scripts/scripts.js
+++ client/assets/scripts/scripts.js
@@ -1,26 +1,19 @@
 //  ****use this for random image
-
 const clickedButton = document.querySelectorAll(".click");
-
 const container = document.querySelector(".content");
 
 function displayData(data) {
   const btnTwo = document.querySelector("#btn-2");
 
-  btnTwo.addEventListener("click", (e) => {
-    let randomId = Math.floor(Math.random() * data.length);
-    console.log("hello");
-    container.innerHTML = `<figure><img src="${data[randomId].persian}" alt="">`; //****Solve random image problem
-  });
-
   const loopData = data.map((elem) => {
     clickedButton.forEach((item) => {
       item.addEventListener("click", () => {
         let search = document.querySelector("#input").value;
-
-        console.log(data.img);
+        let randomId = Math.floor(Math.random() * data.length);
         if (item.id == "btn-1" && search > "") {
           container.innerHTML += `<figure><img src="${elem[search]}" alt=""><figcaption>${elem.name}</figcaption></figure>`;
+        } else if (item.id == "btn-2") {
+          container.innerHTML = `<figure><img src="${data[randomId].persian}" alt="">`; //****Solve random image problem
         } else {
           container.innerHTML = `<p>You must enter a search term!</p>`;
         }
@@ -36,9 +29,10 @@ async function getData() {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.data);
     displayData(data);
   } catch (error) {
     alert("Sorry! Your request could not be granted!");
   }
 }
+
+getData();
