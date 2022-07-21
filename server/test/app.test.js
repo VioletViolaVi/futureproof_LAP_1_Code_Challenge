57945df163874266dcf1fc73c5c42b17135fbe19
vivian <vivian.v18723@gmail.com>
2022-07-21 17:41:42 +0100

    add simple tests

---
+++ server/test/app.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+
+const app = require("../app");
+
+describe("API", () => {
+  let api;
+
+  beforeAll(() => {
+    api = app.listen(3030);
+  });
+
+  afterAll((done) => {
+    api.close(done);
+  });
+
+  it("Responds to a GET request at / with a 200 status", (done) => {
+    request(api).get("/").expect(200, done);
+  });
+
+  it("Responds to a GET request at /data with a 200 status", (done) => {
+    request(api).get("/data").expect(200, done);
+  });
+
+  it("Responds to a GET request at /data with a JSON object", (done) => {
+    request(api).get("/data").expect("Content-Type", /json/, done);
+  });
+});
