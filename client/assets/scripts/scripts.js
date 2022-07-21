c9add1a0059d88eda43dd87dcd8c8b449b7728aa
vivian <vivian.v18723@gmail.com>
2022-07-21 12:13:45 +0100

    edit css & js files

--- client/assets/scripts/scripts.js
+++ client/assets/scripts/scripts.js
@@ -1,28 +1,27 @@
-//  ****use this for random image
 const clickedButton = document.querySelectorAll(".click");
 const container = document.querySelector(".content");
-
 function displayData(data) {
   const loopData = data.map((elem) => {
     clickedButton.forEach((item) => {
       item.addEventListener("click", () => {
         let search = document.querySelector("#input").value;
         let randomId = Math.floor(Math.random() * data.length);
-        if (item.id == "btn-1" && search > "") {
+        if (item.id == "btn-1" && search == "persian") {
           container.innerHTML += `<figure><img src="${elem[search]}" alt=""><figcaption>${elem.name}</figcaption></figure>`;
-        } else if (item.id == "btn-2") {
-          container.innerHTML = `<figure><img src="${data[randomId].persian}" alt="${data[randomId].name}"><figcaption>${data[randomId].name}</figcaption></figure>`; //****Solve random image problem
         } else {
-          container.innerHTML = `<p>You must enter a search term (persian)!</p>`;
+          document.querySelector("#input").value = "";
+          document.querySelector("#input").placeholder =
+            "Enter a valid search term (persian)!";
+        }
+        if (item.id == "btn-2") {
+          container.innerHTML = `<figure><img src="${data[randomId].persian}" alt="${data[randomId].name}"><figcaption>${data[randomId].name}</figcaption></figure>`;
         }
       });
     });
   });
 }
-
 // Define the url
 const url = "http://localhost:3000/data";
-
 async function getData() {
   try {
     const response = await fetch(url);
@@ -32,5 +31,4 @@ async function getData() {
     alert("Sorry! Your request could not be granted!");
   }
 }
-
 getData();
